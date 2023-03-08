475a49bf609678d19d58cf3c204d2be4e3d81f5c
dushyantlamyan48 <126569623+dushyantlamyan48@users.noreply.github.com>
2023-03-08 16:21:52 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.set("port", process.env.PORT || 4000);
+
+app.get('/vcc', function (req, res) {
+   res.writeHead(200, {'Content-Type': 'application/json'});
+   var response = { "response" : "This is GET method." }
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+app.get('/vcc/1.0/token', function (req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var response = { "response" : "This is GET token method." }
+  console.log(response);
+  res.end(JSON.stringify(response));
+})
+
+app.post('/vcc/1.0/token', function (req, res) {
+  //console.log(req.body);
+  //res.writeHead(200, {'Content-Type': 'application/json'});
+  var response = { "response" : "This is token POST method." }
+  console.log(response);
+  res.send({
+          "access_token": "eyJz93k4laUWw",
+          "token_type": "Bearer asc",
+          "expires_in": 3600,
+  });
+})
+
+app.post('/vcc/1.0/posts/:postId/messages', async function (req, res) {
+  console.log(req.body);
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var response = { "response" : "This is messages POST method." }
+  console.log(response);
+  res.end(JSON.stringify(response));
+})
+
+app.post('/vcc/2.0/channel/:channelId/outbound', function (req, res) {
+  console.log(req.body);
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var response = { "response" : "This is outbound POST method." }
+  console.log(response);
+  res.end(JSON.stringify(response));
+})
+
+var server = app.listen(app.get("port"), function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Node.js API app listening at http://%s:%s", host, port)
+})
